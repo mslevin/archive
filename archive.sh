f36f823a0af34ce1c1873b2f3e6315c64f4e1607
Michael Slevin <michaelslevin11@gmail.com>
2014-02-02 11:55:00 -0800

    added archive file

---
+++ archive.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# archive.sh - a script to "archive" files you don't need anymore
+# Usage: ./archive [filename]
+
+# This script makes a new folder in the directory that you're currently in called "Old".
+# If it already exists, it does nothing.
+# It makes a copy of the file specified, adds OLD onto the end, and moves it into the Old folder.
+# If there is already a file called [filename]OLD, it will rename it to [filename]OldX,
+# where X is a number starting at 2. If [filename]OLD2 already exists, it would be 3, etc.
+
+FILENAME=$1;
+TIME=$(date +"%H:%M_%m_%d_%Y")
+
+# If Old/ doesn't exist, make it!
+if [ ! -d "Old" ]; then
+   mkdir Old
+fi
+
+cp $FILENAME $FILENAME-$TIME
+mv "$FILENAME-$TIME" "Old"
+echo "File archived"
+
+
+
+
