1bc11ef96e070ff2c36cb20155a225c62252ac60
Michael Slevin <michaelslevin11@gmail.com>
2014-03-04 13:45:12 -0800

    Changed some stuff. The script now creates a folder inside the "Old" folder it makes with the date and then puts the file in there with the time appended to the filename. I think its better organized this way. Still playing around with it though.

--- archive.sh
+++ archive.sh
@@ -10,15 +10,20 @@
 # where X is a number starting at 2. If [filename]OLD2 already exists, it would be 3, etc.
 
 FILENAME=$1;
-TIME=$(date +"%H:%M_%m_%d_%Y")
+TIME=$(date +"%H:%M")
+DAY=$(date +"%m_%d_%Y")
 
 # If Old/ doesn't exist, make it!
 if [ ! -d "Old" ]; then
    mkdir Old
 fi
 
+if [ ! -d "Old/$DAY" ]; then
+   mkdir Old/$DAY
+fi
+
 cp $FILENAME $FILENAME-$TIME
-mv "$FILENAME-$TIME" "Old"
+mv "$FILENAME-$TIME" "Old/$DAY"
 echo "File archived"
 
 
